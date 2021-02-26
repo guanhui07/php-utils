4d0e1f8b9da25461024f442ef584b10ba0183617
linshan <1099013371@qq.com>
2021-02-26 14:13:44 +0800

    init utils

---
+++ README.md
@@ -0,0 +1,28 @@
+# utils 基础服务工具包
+
+[![GitHub release](https://img.shields.io/github/release/raylin666/utils.svg)](https://github.com/raylin666/utils/releases)
+[![PHP version](https://img.shields.io/badge/php-%3E%207.2-orange.svg)](https://github.com/php/php-src)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](#LICENSE)
+
+承载 通用/复用 方法函数、类 , 方便轻松调用, 后续将不断添加通用类函数。
+
+### 环境要求
+
+* PHP >=7.2
+
+### 安装说明
+
+```
+composer require "raylin666/utils"
+```
+
+## 更新日志
+
+请查看 [CHANGELOG.md](CHANGELOG.md)
+
+### 联系
+
+如果你在使用中遇到问题，请联系: [1099013371@qq.com](mailto:1099013371@qq.com). 博客: [kaka 梦很美](http://www.ls331.com)
+
+## License MIT
+
---
+++ helpers.php
@@ -0,0 +1,119 @@
+<?php
+// +----------------------------------------------------------------------
+// | Created by linshan. 版权所有 @
+// +----------------------------------------------------------------------
+// | Copyright (c) 2020 All rights reserved.
+// +----------------------------------------------------------------------
+// | Technology changes the world . Accumulation makes people grow .
+// +----------------------------------------------------------------------
+// | Author: kaka梦很美 <1099013371@qq.com>
+// +----------------------------------------------------------------------
+
+use Raylin666\Util\Helper\PhpHelper;
+use Raylin666\Util\Coroutine;
+use Swoole\Runtime as SwooleRuntime;
+use Swoole\Coroutine as SwooleCoroutine;
+use Raylin666\Util\ApplicationContext;
+
+if (! function_exists('container'))
+{
+    /**
+     * 获取容器服务
+     * @return \Raylin666\Contract\ContainerInterface
+     * @throws Exception
+     */
+    function container()
+    {
+        if (ApplicationContext::hasContainer()) {
+            return ApplicationContext::getContainer();
+        }
+
+        throw new Exception('The service container is not registered. Please execute `\Raylin666\Util\ApplicationContext::setContainer` Registration completed.');
+    }
+}
+
+if (! function_exists('make'))
+{
+    /**
+     * Create a object instance, if the DI container exist in ApplicationContext,
+     * then the object will be create by DI container via `make()` method, if not,
+     * the object will create by `new` keyword.
+     *
+     * @param string $name
+     * @param array  $parameters
+     * @return mixed
+     */
+    function make(string $name, array $parameters = [])
+    {
+        if (ApplicationContext::hasContainer()) {
+            $container = ApplicationContext::getContainer();
+            if (method_exists($container, 'make')) {
+                return $container->make($name, $parameters);
+            }
+        }
+
+        return new $name(...array_values($parameters));
+    }
+}
+
+if (! function_exists('call'))
+{
+    /**
+     * 调用函数
+     * @param       $callback
+     * @param array $args
+     * @return mixed
+     */
+    function call($callback, array $args = [])
+    {
+        return PhpHelper::call($callback, ...$args);
+    }
+}
+
+if (! function_exists('run'))
+{
+    /**
+     * Run callable in non-coroutine environment, all hook functions by Swoole only available in the callable.
+     *
+     * @param array|callable $callbacks
+     */
+    function run($callbacks, $flags = SWOOLE_HOOK_ALL): bool
+    {
+        if (Coroutine::inCoroutine()) {
+            throw new RuntimeException('Function \'run\' only execute in non-coroutine environment.');
+        }
+
+        SwooleRuntime::enableCoroutine(true, $flags);
+
+        $result = SwooleCoroutine\run(...(array) $callbacks);
+
+        SwooleRuntime::enableCoroutine(false);
+
+        return $result;
+    }
+}
+
+if (! function_exists('go'))
+{
+    /**
+     * 创建协程
+     * @return bool|int
+     */
+    function go(callable $callable)
+    {
+        $id = Coroutine::create($callable);
+        return $id > 0 ? $id : false;
+    }
+}
+
+if (! function_exists('swoole_enable_coroutine'))
+{
+    /**
+     * 开启/设置 Swoole 协程 Hook
+     * @param $flags
+     */
+    function swoole_enable_coroutine($flags = SWOOLE_HOOK_ALL)
+    {
+        SwooleRuntime::enableCoroutine(true, $flags);
+    }
+}
